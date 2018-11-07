2fe556d83208e268d48c48bd395c8bb8c18db94a
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2018-11-07 10:41:22 +0000

    Add verbosity to DataOut

--- UserTools/DataOut/DataOut.cpp
+++ UserTools/DataOut/DataOut.cpp
@@ -8,7 +8,11 @@ bool DataOut::Initialise(std::string configfile, DataModel &data){
   if(configfile!="")  m_variables.Initialise(configfile);
   //m_variables.Print();
 
+  verbose = 0;
+  m_variables.Get("verbose", verbose);
+
   m_data= &data;
+
 
   return true;
 }
