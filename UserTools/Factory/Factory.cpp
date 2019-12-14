05441747918aa8d06d87e452836a9fdea72250d5
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2019-12-14 19:50:48 +0100

    Add BONSAI to the factory (and sort the whitespace out)

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -1,17 +1,19 @@
 #include "../Unity.cpp"
 
 Tool* Factory(std::string tool){
-Tool* ret=0;
+  Tool* ret=0;
 
-// if (tool=="Type") tool=new Type;
-if (tool=="DummyTool") ret=new DummyTool;
+  // if (tool=="Type") tool=new Type;
+  if (tool=="DummyTool") ret=new DummyTool;
 
-if (tool=="TriggerOutput") ret=new TriggerOutput;
-if (tool=="WCSimASCIReader") ret=new WCSimASCIReader;
-if (tool=="nhits") ret=new nhits;
-if (tool=="test_vertices") ret=new test_vertices;
-if (tool=="WCSimReader") ret=new WCSimReader;
-if (tool=="DataOut") ret=new DataOut;
-return ret;
+  if (tool=="TriggerOutput") ret=new TriggerOutput;
+  if (tool=="WCSimASCIReader") ret=new WCSimASCIReader;
+  if (tool=="nhits") ret=new nhits;
+  if (tool=="test_vertices") ret=new test_vertices;
+  if (tool=="WCSimReader") ret=new WCSimReader;
+  if (tool=="DataOut") ret=new DataOut;
+  if (tool=="BONSAI") ret=new BONSAI;
+
+  return ret;
 }
 
