68da471b6d2c1d458b7554eac7a8d00cc9f81819
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2019-12-18 18:51:00 +0100

    Split ReconInfo into .cpp file

--- DataModel/ReconInfo.h
+++ DataModel/ReconInfo.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 typedef enum EReconstructers {
   kReconUndefined = -1,
@@ -32,109 +33,20 @@ struct CherenkovCone
 class ReconInfo
 {
  public:
- ReconInfo() : fNRecons(0), fFirstTime(+9E20), fLastTime(-9E20) {}
+  ReconInfo();
 
-  void AddRecon(Reconstructer_t reconstructer, int trigger_num, int nhits, double time, double * vertex, double goodness_of_fit, double goodness_of_time_fit, bool fill_has_direction = true) {
-    if(fill_has_direction) {
-      if(ShouldProvideDirection(reconstructer)) {
-	std::cerr << "Reconstructer " << ReconInfo::EnumAsString(reconstructer) << " provides direction information but we are filling reconstruction information without it" << std::endl;
-      }
-      fHasDirection.push_back(false);
-    }
-    fReconstructer.push_back(reconstructer);
-    fTriggerNum.push_back(trigger_num);
-    fNHits.push_back(nhits);
-    fTime.push_back(time);
-    Pos3D pos;
-    pos.x = vertex[0];
-    pos.y = vertex[1];
-    pos.z = vertex[2];
-    fVertex.push_back(pos);
-    fGoodnessOfFit.push_back(goodness_of_fit);
-    fGoodnessOfTimeFit.push_back(goodness_of_time_fit);
-    fNRecons++;
-    UpdateTimeBoundaries(time);
-  }
+  void AddRecon(Reconstructer_t reconstructer, int trigger_num, int nhits, double time, double * vertex, double goodness_of_fit, double goodness_of_time_fit, bool fill_has_direction = true);
  
   void AddRecon(Reconstructer_t reconstructer, int trigger_num, int nhits, double time, double * vertex, double goodness_of_fit, double goodness_of_time_fit,
-		double * direction_euler, double * cherenkov_cone, double direction_likelihood) {
-    AddRecon(reconstructer, trigger_num, nhits, time, vertex, goodness_of_fit, goodness_of_time_fit, false);
-    fHasDirection.push_back(true);
-    DirectionEuler direct;
-    direct.theta = direction_euler[0];
-    direct.phi   = direction_euler[1];
-    direct.alpha = direction_euler[2];
-    fDirectionEuler.push_back(direct);
-    CherenkovCone cone;
-    cone.cos_angle = cherenkov_cone[0];
-    cone.ellipticity = cherenkov_cone[1];
-    fCherenkovCone.push_back(cone);
-    fDirectionLikelihood.push_back(direction_likelihood);
-  }
+		double * direction_euler, double * cherenkov_cone, double direction_likelihood);
 
-  void AddReconFrom(ReconInfo * in, const int irecon) {
-    fReconstructer.push_back(in->GetReconstructer(irecon));
-    fTriggerNum.push_back(in->GetTriggerNum(irecon));
-    fNHits.push_back(in->GetNHits(irecon));
-    fTime.push_back(in->GetTime(irecon));
-    fVertex.push_back(in->GetVertex(irecon));
-    fGoodnessOfFit.push_back(in->GetGoodnessOfFit(irecon));
-    fGoodnessOfTimeFit.push_back(in->GetGoodnessOfTimeFit(irecon));
-    fHasDirection.push_back(in->GetHasDirection(irecon));
-    if(in->GetHasDirection(irecon)) {
-      fDirectionEuler.push_back(in->GetDirectionEuler(irecon));
-      fCherenkovCone.push_back(in->GetCherenkovCone(irecon));
-      fDirectionLikelihood.push_back(in->GetDirectionLikelihood(irecon));
-    }
-    fNRecons++;
-    UpdateTimeBoundaries(in->GetTime(irecon));
-  }
+  void AddReconFrom(ReconInfo * in, const int irecon);
 
-  static std::string EnumAsString(Reconstructer_t r) {
-    switch(r) {
-    case (kReconBONSAI):
-      return "BONSAI";
-      break;
-    case (kReconTestVerticesNoDirection):
-      return "TestVertices_NoDirection";
-      break;
-    case (kReconTestVertices):
-      return "TestVertices";
-      break;
-    case(kReconRandomNoDirection):
-      return "Random_NoDirection";
-      break;
-    case (kReconRandom):
-      return "Random";
-      break;
-    default:
-      return "";
-    }
-    return "";
-  }
+  static std::string EnumAsString(Reconstructer_t r);
   
-  static Reconstructer_t ReconstructerFromString(std::string s) {
-    for(int i = int(kReconUndefined)+1; i <= kReconRandom; i++) {
-      if(s.compare(ReconInfo::EnumAsString((Reconstructer_t)i)) == 0) {
-	return (Reconstructer_t)i;
-      }
-    }
-    std::cerr << "ReconInfo::ReconstructerFromString() Unknown string value " << s << std::endl;
-    return kReconUndefined;
-  }
+  static Reconstructer_t ReconstructerFromString(std::string s);
 
-  bool ShouldProvideDirection(Reconstructer_t r) {
-    switch(r) {
-    case(kReconBONSAI):
-    case(kReconTestVertices):
-    case(kReconRandom):
-      return true;
-      break;
-    default:
-      return false;
-    }
-    return false;
-  }
+  static bool ShouldProvideDirection(Reconstructer_t r);
 
   int             GetNRecons  () { return fNRecons;   }
   double          GetFirstTime() { return fFirstTime; }
@@ -152,25 +64,7 @@ class ReconInfo
   CherenkovCone   GetCherenkovCone      (int irecon) { return fCherenkovCone[irecon]; }
   double          GetDirectionLikelihood(int irecon) { return fDirectionLikelihood[irecon]; }
   
-  void Reset() {
-    fNRecons = 0;
-    fFirstTime = +9E20;
-    fLastTime = -9E20;
-    //event
-    fReconstructer.clear();
-    fTriggerNum.clear();
-    fNHits.clear();
-    //vertex
-    fTime.clear();
-    fVertex.clear();
-    fGoodnessOfFit.clear();
-    fGoodnessOfTimeFit.clear();
-    //direction
-    fHasDirection.clear();
-    fDirectionEuler.clear();
-    fCherenkovCone.clear();
-    fDirectionLikelihood.clear();
-  }
+  void Reset();
 
  private:
 
@@ -196,12 +90,8 @@ class ReconInfo
   std::vector<CherenkovCone>   fCherenkovCone;
   std::vector<double>          fDirectionLikelihood;
 
-  void UpdateTimeBoundaries(double time) {
-    if(time < fFirstTime)
-      fFirstTime = time;
-    if(time > fLastTime)
-      fLastTime = time;
-  }
+  void UpdateTimeBoundaries(double time);
+
 };
 
 #endif //RECONINFO_H
